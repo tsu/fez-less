6d8d98e71a92d119d6e3d2cfdb9e10e9b7243219
Isaac Wagner <isaacbwagner@gmail.com>
2014-01-03 23:20:27 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var Parser = less = require("less").Parser,
+    Promise = require("promise");
+
+module.exports = function(options) {
+  var parser = new Parser(options);
+
+  function lessp(p) {
+    return p.then(function(input){ 
+      return new Promise(function(resolve, reject) {
+	parser.parse(input.toString(), function (err, tree) { 
+	  if(err) reject(err); 
+	  else resolve(tree.toCSS()); 
+	});
+      });
+    });
+  };
+
+  return function(inputs) {
+    return Promise.all(inputs.map(function(input) { return input.asBuffer(); }).map(lessp)).then(function(css) { return css.join(""); });
+  };
+};
+
